e17c9d3f48a85f69d5b5980f4b9a26b5ce42f667
arunraj3 <arunrajgovindan2002@gmail.com>
2023-07-12 22:00:30 +0530

    update

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
